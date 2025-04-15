620f25009237458a5adf10a85459d017ea700ffe
iXendeRouS <aharrison9608@gmail.com>
2025-04-16 09:14:05 +1200

    Updated version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace SandboxSaver;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "SandboxSaver";
 
     public const string Description = "Press M to make and L to load sandbox saves by name.";
