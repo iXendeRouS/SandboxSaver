9cbdfc61508b62729c8b3f3447703bf2518c469e
iXendeRouS <aharrison9608@gmail.com>
2025-05-08 21:37:50 +1200

    Updated version, description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@ namespace SandboxSaver;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "1.0.1";
+    public const string Version = "1.1.0";
     public const string Name = "SandboxSaver";
 
-    public const string Description = "Press M to make and L to load sandbox saves by name.";
+    public const string Description = "Make saves of games to load them in sandbox.";
 
     public const string RepoOwner = "iXendeRouS";
     public const string RepoName = "SandboxSaver";
